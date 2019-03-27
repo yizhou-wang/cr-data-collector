516322080ccde91bcc660a7c251eca95fb7f4db2
Yizhou Wang <wyzh555@gmail.com>
2019-03-26 19:32:50 -0700

    add user-friendly notations

--- Radar_mousehook.py
+++ Radar_mousehook.py
@@ -2,6 +2,7 @@ from pymouse import PyMouse
 from pykeyboard import PyKeyboard
 import time
 import os
+import shutil
 
 m = PyMouse()
 k = PyKeyboard()
@@ -10,12 +11,18 @@ x_dim, y_dim = m.screen_size()
 # click DCA1000RAM
 m.click(x_dim//2-150, y_dim//2-120, 1)
 ## record the click time and click trigger
-t0 = time.clock()
 m.click(x_dim//2-70, y_dim//2-100, 1)
 
 t1 = time.time()
 ## save time t1
 
-print('Finished?')
+print('Finished Recording Data? Y/N')
+# if yes, continue
+
+time_modif = os.path.getmtime('C:/ti/mmwave_studio_01_00_00_00/mmWaveStudio/PostProc/adc_data_Raw_0.bin')
+print('The creation time of this radar data is ',time_modif,'. Is this correct?')
+# if yes, continue
 ## then packet reorder and zero fill
-## then copy file to destimation
+os.system('cd C:/ti/mmwave_studio_01_00_00_00/mmWaveStudio/PostProc')
+os.system('Packet_Reorder_Zerofill.exe adc_data_Raw_0.bin adc_data.bin')
+## is success, then copy file to destimation; otherwise, print error
