569b1041376da1c13a30d10d64226714ae8d0a96
Yizhou Wang <ywang26@uw.edu>
2019-07-01 05:47:01 +0800

    add multiple camera scripts

--- collector/__init__.py
+++ collector/__init__.py
@@ -1,4 +1,5 @@
 from .cam_driver import run_single_camera
+from .cam_mul_driver import run_multiple_cameras
 from .radar_driver import copy_radar_data
 from .radar_driver import run_radar
 from .radar_driver import init_radar
--- run_datacol.py
+++ run_datacol.py
@@ -5,10 +5,11 @@ import time
 import datetime
 from argparse import ArgumentParser
 
-from collector import run_single_camera
+from collector import run_single_camera, run_multiple_cameras
 from collector import copy_radar_data
 
-def main(base_dir, seq_name, frame_rate, num_img):
+
+def main(base_dir, seq_name, frame_rate, num_img, syn=True):
     """
     Example entry point; please see Enumeration example for more in-depth
     comments on preparing and cleaning up the system.
@@ -21,6 +22,10 @@ def main(base_dir, seq_name, frame_rate, num_img):
 
     # Retrieve singleton reference to system object
     system = PySpin.System.GetInstance()
+
+    # Get current library version
+    version = system.GetLibraryVersion()
+    print('Library version: %d.%d.%d.%d' % (version.major, version.minor, version.type, version.build))
 
     # Retrieve list of cameras from the system
     cam_list = system.GetCameras()
@@ -42,24 +47,26 @@ def main(base_dir, seq_name, frame_rate, num_img):
         input('Done! Press Enter to exit...')
         return False
 
-    # Run example on each camera
-    for i, cam in enumerate(cam_list):
+    if syn or num_cameras == 1:
+        # Run example on each camera
+        for i, cam in enumerate(cam_list):
 
-        print('Running example for camera %d...' % i)
+            print('Running example for camera %d...' % i)
 
-        result &= run_single_camera(cam, seq_dir, frame_rate, num_img)
-        print('Camera %d example complete... \n' % i)
+            result &= run_single_camera(cam, seq_dir, frame_rate, num_img)
+            print('Camera %d example complete... \n' % i)
 
-    # # Init radar
-    # engine = init_radar()
-    # # Run radar
-    # run_radar(engine)
+        # Release reference to camera
+        # NOTE: Unlike the C++ examples, we cannot rely on pointer objects being automatically
+        # cleaned up when going out of scope.
+        # The usage of del is preferred to assigning the variable to None.
+        del cam
 
-    # Release reference to camera
-    # NOTE: Unlike the C++ examples, we cannot rely on pointer objects being automatically
-    # cleaned up when going out of scope.
-    # The usage of del is preferred to assigning the variable to None.
-    del cam
+    else:
+        # Run example on all cameras
+        print('Running example for all cameras...')
+
+        result = run_multiple_cameras(cam_list)
 
     # Clear camera list before releasing system
     cam_list.Clear()
