880b4965fe34453599bec40be9a0c3300259bcf5
Yizhou Wang <ywang26@uw.edu>
2019-09-26 18:13:46 -0700

    Create cam_stereo_rectify.py

---
+++ preprocess/cam_stereo_rectify.py
@@ -0,0 +1,113 @@
+import os
+import numpy as np
+import cv2
+import yaml
+import datetime
+
+
+def load_calib(calib_yaml):
+    with open(calib_yaml, "r") as stream:
+        data = yaml.safe_load(stream)
+        image_width = data['image_width']
+        image_height = data['image_height']
+        camera_matrix = data['camera_matrix']
+        distortion_coefficients = data['distortion_coefficients']
+        rectification_matrix = data['rectification_matrix']
+        projection_matrix = data['projection_matrix']
+
+        K = np.array(camera_matrix['data'])
+        K = np.reshape(K, (camera_matrix['rows'], camera_matrix['cols']))
+        D = np.array(distortion_coefficients['data'])
+        D = np.reshape(D, (distortion_coefficients['rows'], distortion_coefficients['cols']))
+        D = np.squeeze(D)
+        R = np.array(rectification_matrix['data'])
+        R = np.reshape(R, (rectification_matrix['rows'], rectification_matrix['cols']))
+        P = np.array(projection_matrix['data'])
+        P = np.reshape(P, (projection_matrix['rows'], projection_matrix['cols']))
+
+    return image_width, image_height, K, D, R, P
+
+
+def undistort(im_name, K, D):
+
+    im = cv2.imread(im_name)
+    undistort = cv2.undistort(im, K, D)
+
+    return undistort
+
+
+def rectify_for_seq(folder_dir_l, folder_dir_r, calib_yaml_l, calib_yaml_r):
+
+    im_names_l = sorted(os.listdir(folder_dir_l))
+    w_l, h_l, K_l, D_l, R_l, P_l = load_calib(calib_yaml_l)
+    folder_out_dir_l = folder_dir_l.replace('images', 'images_udst')
+    im_names_r = sorted(os.listdir(folder_dir_r))
+    w_r, h_r, K_r, D_r, R_r, P_r = load_calib(calib_yaml_r)
+    folder_out_dir_r = folder_dir_r.replace('images', 'images_udst')
+
+    assert len(im_names_l) == len(im_names_r)
+    assert w_l == w_r
+    assert h_l == h_r
+
+    if not os.path.exists(folder_out_dir_l):
+        os.makedirs(folder_out_dir_l)
+    if not os.path.exists(folder_out_dir_r):
+        os.makedirs(folder_out_dir_r)
+
+    #computes undistort and rectify maps
+    mapxL, mapyL = cv2.initUndistortRectifyMap(K_l, D_l, R_l, P_l, (w_l, h_l), cv2.CV_32FC1)
+    mapxR, mapyR = cv2.initUndistortRectifyMap(K_r, D_r, R_r, P_r, (w_r, h_r), cv2.CV_32FC1)
+
+    for im_name_l, im_name_r in zip(im_names_l, im_names_r):
+        im_dir_l = os.path.join(folder_dir_l, im_name_l)
+        im_dir_r = os.path.join(folder_dir_r, im_name_r)
+
+        lFrame = cv2.imread(im_dir_l)
+        rFrame = cv2.imread(im_dir_r)
+        dstL = cv2.remap(lFrame, mapxL, mapyL,cv2.INTER_LINEAR)
+        dstR = cv2.remap(rFrame, mapxR, mapyR,cv2.INTER_LINEAR)
+
+        im_out_name_l = os.path.join(folder_out_dir_l, im_name_l)
+        im_out_name_r = os.path.join(folder_out_dir_r, im_name_r)
+        cv2.imwrite(im_out_name_l, dstL)
+        cv2.imwrite(im_out_name_r, dstR)
+
+
+def undistort_for_date(date, data_dir='D:\\RawData'):
+
+    base_dir = os.path.join(data_dir, date)
+
+    if not os.path.exists(base_dir):
+        raise ValueError("Data not found!")
+
+    # calib_yaml = os.path.join(base_dir, 'cam_calib.yaml')
+    # if not os.path.exists(calib_yaml):
+    #     print("Specific calibration for this date is unavailable, using default value.\n")
+    #     calib_yaml = 'ost.yaml'
+
+    calib_yaml_l = os.path.join(data_dir, 'calib', '2019_09_26_18384019-19325055', 'left.yaml')
+    calib_yaml_r = os.path.join(data_dir, 'calib', '2019_09_26_18384019-19325055', 'right.yaml')
+    seqs = sorted(os.listdir(base_dir))
+    for seq in seqs:
+        folder_dir_l = os.path.join(base_dir, seq, 'images_0')
+        folder_dir_r = os.path.join(base_dir, seq, 'images_1')
+        print("Rectifying %s ..." % folder_dir_l)
+        rectify_for_seq(folder_dir_l, folder_dir_r, calib_yaml_l, calib_yaml_r)
+
+    print("\nStereo rectification finished for date %s." % date)
+
+
+if __name__ == '__main__':
+
+    # main("D:/RawData/0000/images", 'ost.yaml')
+    # load_calib('ost.yaml')
+
+    data_root = input("Enter root directory (default=D:\\RawData): ")
+    if data_root == '':
+        data_root = 'D:\\RawData'
+    date = input("Enter date (default=today): ")
+    if date == '':
+        now = datetime.datetime.now()
+        date = "%s_%02d_%02d" % (now.year, now.month, now.day)
+
+    undistort_for_date(date, data_dir=data_root)
